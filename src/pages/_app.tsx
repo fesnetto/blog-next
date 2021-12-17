6fe0caf85ac1d520d63bf1320fddbdf4b2750335
fesnetto <fernando.esnetto@gmail.com>
2021-12-17 17:49:00 +0000

    Configured Styled Components, deleted css archives, modified _app.tsx, index.tsx and created babelrc.js e _document.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/dist/shared/lib/router/router'
+import { GlobalStyles } from '../styles/global-styles'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    < >
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default MyApp
