e50837009a03df576ec8daf57b931cb8fa2e2dc3
fesnetto <fernando.esnetto@gmail.com>
2021-12-17 18:12:20 +0000

    Created theme.ts in styles path and imported it into _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import { AppProps } from 'next/dist/shared/lib/router/router'
+import { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../styles/global-styles'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    < >
+    < ThemeProvider theme={theme} >
       <Component {...pageProps} />
       <GlobalStyles />
-    </>
+    </ThemeProvider>
   );
 }
 
