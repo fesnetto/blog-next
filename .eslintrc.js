914bc1dd9cb8b1ade7a0ac2ac94008ab76e0eea9
fesnetto <fernando.esnetto@gmail.com>
2021-12-17 11:42:37 +0000

    Modified .eslintrc, created .prettier and path src to paste pages and styles path. Removed api path

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended"
+    'plugin:prettier/recommended'
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
