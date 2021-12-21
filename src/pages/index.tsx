c51806c4321cf16e17034349c3a1af22ada84f0e
fesnetto <fernando.esnetto@gmail.com>
2021-12-21 09:52:00 +0000

    Modified global-styles.ts and index.tsx to show titles of posts of blog-strapi, but didn't work yet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,33 @@
-export default function Home() {
-  return <h1>Oi</h1>
+import { GetStaticProps } from 'next';
+import { PostData } from '../domain/posts/post';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch(
+    'http://localhost:1337/api/posts',
+  );
+  const jsonPosts = await posts.json();
+  return jsonPosts;
+};
+
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.slug}>{post.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: { posts },
+    // revalidate: 5,
+  };
+};
