1458fb0091a3cbdef5f341579e4e4fec9b7505f1
fesnetto <fernando.esnetto@gmail.com>
2021-12-22 10:21:05 +0000

    Created MainContainer page and styled

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -12,11 +12,11 @@ export const GlobalStyles = createGlobalStyle`
     font-size: 62.5%;  // Esse valor é para ajustar o valor de px com rem, assim 1 rem é igual a 10px
   }
   body {
-    font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: ${({ theme }) => theme.font.sizes.medium}
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: ${() => theme.font.sizes.medium}
   }
   a {
-    color: ${({ theme }) => theme.colors.secondary};
+    color: ${() => theme.colors.secondary};
     text-decoration: none;
     transition: opacity 300ms ease-in-out;
     &:hover {
