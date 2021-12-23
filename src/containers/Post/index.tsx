373f5ebdc7da494f78fb801e9a13335e9d00b462
fesnetto <fernando.esnetto@gmail.com>
2021-12-23 12:10:00 +0000

    Created Date, Heading, PostCover, PostDetails components and utils/format-date file

---
+++ src/containers/Post/index.tsx
@@ -0,0 +1,32 @@
+import { Footer } from '../../components/Footer';
+import { Header } from '../../components/Header';
+import { MainContainer } from '../../components/MainContainer';
+import { PostData } from '../../domain/posts/post';
+import { Heading } from '../../components/Heading';
+import { PostCover } from '../../components/PostCover';
+import { PostDetails } from '../../components/PostDetails';
+
+export type PostProps = {
+  post: PostData;
+};
+
+export const Post = ({ post }: PostProps) => {
+  return (
+    <>
+      <Header />
+
+      <MainContainer>
+        <Heading>{post.title}</Heading>
+        <PostCover coverUrl={post.cover.formats.large.url} alt={post.title} />
+        <PostDetails
+          author={post.author.name}
+          category={post.category.name}
+          date={post.created_at}
+        />
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </MainContainer>
+
+      <Footer />
+    </>
+  );
+};
