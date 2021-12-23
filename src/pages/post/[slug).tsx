0134b5b4c296fec3c999e0a3678da8cad9d3ae8d
fesnetto <fernando.esnetto@gmail.com>
2021-12-23 11:40:59 +0000

    Created Post folder, instaled Remark to convert markdown, modified [slug]

--- src/pages/post/[slug).tsx
+++ src/pages/post/[slug).tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { Post } from '../../containers/Post';
 import { countAllPosts } from '../../data/posts/count-all-posts';
 import { getAllPosts } from '../../data/posts/get-all-posts';
 import { getPost } from '../../data/posts/get-post';
@@ -9,7 +10,7 @@ export type DynamicPostProps = {
 };
 
 const DynamicPost = ({ post }: DynamicPostProps) => {
-  return <p>{post.title}</p>;
+  return <Post post={post} />;
 };
 
 export default DynamicPost;
